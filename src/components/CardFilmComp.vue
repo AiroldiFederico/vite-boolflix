19767faeb092188385d7f251b5f0e74e13fe0fca
Federico Airoldi <airoldibusiness@gmail.com>
2023-05-14 02:07:30 +0200

    FASE 4 - sistemo lo stile

--- src/components/CardFilmComp.vue
+++ src/components/CardFilmComp.vue
@@ -29,34 +29,38 @@
 
         <h2>MOVIE</h2>
         <div class="card-container">
-            <div class="card" style="width: 18rem;" v-for="(object, index) in store.MovieFind" key="index">
-                <img :src="`https://image.tmdb.org/t/p/w342${store.MovieFind[index].backdrop_path}`" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">{{store.MovieFind[index].title}}</h5>
-                  <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+            <div  v-for="(object, index) in store.MovieFind" key="index">
+                <div class="card" style="width: 18rem;">
+                    <img :src="`https://image.tmdb.org/t/p/w342${store.MovieFind[index].poster_path}`" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{store.MovieFind[index].title}}</h5>
+                        <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{store.MovieFind[index].original_title}}</li>
+                        <li class="list-group-item">Lingua: {{store.MovieFind[index].original_language}}</li>
+                        <li class="list-group-item">Voto: {{store.MovieFind[index].vote_average}}</li>
+                    </ul>
                 </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">{{store.MovieFind[index].original_title}}</li>
-                  <li class="list-group-item">Lingua: {{store.MovieFind[index].original_language}}</li>
-                  <li class="list-group-item">Voto: {{store.MovieFind[index].vote_average}}</li>
-                </ul>
             </div>
         </div>
 
 
         <h2>SERIES TV</h2>
         <div class="card-container">
-            <div class="card" style="width: 18rem;" v-for="(object, index) in store.SerieFind" key="index">
-                <img :src="`https://image.tmdb.org/t/p/w342${store.SerieFind[index].backdrop_path}`" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">{{store.SerieFind[index].title}}</h5>
-                  <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+            <div  v-for="(object, index) in store.SerieFind" key="index">
+                <div class="card" style="width: 18rem;">
+                    <img :src="`https://image.tmdb.org/t/p/w342${store.SerieFind[index].poster_path}`" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{store.SerieFind[index].title}}</h5>
+                      <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">{{store.SerieFind[index].original_name}}</li>
+                      <li class="list-group-item">Lingua: {{store.SerieFind[index].original_language}}</li>
+                      <li class="list-group-item">Voto: {{store.SerieFind[index].vote_average}}</li>
+                    </ul>
                 </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">{{store.SerieFind[index].original_title}}</li>
-                  <li class="list-group-item">Lingua: {{store.SerieFind[index].original_language}}</li>
-                  <li class="list-group-item">Voto: {{store.SerieFind[index].vote_average}}</li>
-                </ul>
             </div>
         </div>
 
@@ -79,7 +83,12 @@ div {
     .card-container {
         display: flex;
         flex-direction: row;
-        flex-wrap: wrap;
+        flex-wrap: nowrap;
+        overflow: auto;
+
+        .card {
+            height: 46rem;
+        }
     }
 
     h2 {
@@ -88,6 +97,8 @@ div {
         border-top: solid white 0.1rem;
 
     }
+
+
 }
 
 
